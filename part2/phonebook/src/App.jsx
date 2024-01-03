a5a48f0b1c9313a16a1c3ecaca48c2ab4d4b8168
Jayant Ghadge <jayantghadge6@gmail.com>
2024-01-03 21:11:38 +0530

    Added Exercises 2.12 - 2.17 of Part2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import personService from "./services/persons";
+import "./index.css";
 
 const Filter = ({ searchTerm, handleSearchNameChange }) => {
   return (
@@ -31,43 +33,141 @@ const PersonForm = ({
   );
 };
 
-const Persons = ({ filteredPersons }) => {
+const Persons = ({ filteredPersons, handleDelete }) => {
   return (
     <ul>
       {filteredPersons.map((person) => (
         <li key={person.id}>
           {person.name} - {person.number}
+          <button onClick={() => handleDelete(person.id)}>delete</button>
         </li>
       ))}
     </ul>
   );
 };
 
+const Notification = ({ message }) => {
+  if (!message) {
+    return null;
+  }
+
+  const notificationClassName = `notification ${message.type || ""}`;
+  return <div className={notificationClassName}>{message.text}</div>;
+};
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  const [deletedPersons, setDeletedPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchName, setSearchName] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null);
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then((response) => {
+        setPersons(response);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook.`);
+    const existingPerson = persons.find((person) => person.name === newName);
+
+    if (existingPerson) {
+      const isPersonStillPresent = persons.some(
+        (person) => person.id === existingPerson.id
+      );
+
+      if (
+        isPersonStillPresent &&
+        window.confirm(
+          `${newName} is already in the phonebook. Replace the old number with the new one?`
+        )
+      ) {
+        const updatedPerson = { ...existingPerson, number: newNumber };
+        setPersons(
+          persons.map((person) =>
+            person.id === existingPerson.id ? updatedPerson : person
+          )
+        );
+        personService
+          .update(existingPerson.id, updatedPerson)
+          .then((response) => {
+            setPersons(
+              persons.map((person) =>
+                person.id === existingPerson.id ? response : person
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+            showNotification("success", `${newName}'s number was updated.`);
+          })
+          .catch((error) => {
+            console.error("Error updating person:", error);
+            if (error.response && error.response.status === 404) {
+              setPersons(
+                persons.filter((person) => person.id !== existingPerson.id)
+              );
+              showAlertIfPersonDeleted(newName);
+              showNotification(
+                "error",
+                `Information of ${newName} has already been removed from server.`
+              );
+            } else {
+              setNotificationMessage({
+                type: "error",
+                text: "Error updating person. Please try again later.",
+              });
+              setTimeout(() => {
+                setNotificationMessage(null);
+              }, 5000);
+            }
+          });
+      }
     } else {
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       };
 
-      setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+      personService
+        .create(personObject)
+        .then((response) => {
+          setPersons([...persons, response]);
+          setNewName("");
+          setNewNumber("");
+          showAlertIfPersonDeleted(newName);
+          showNotification("success", `Added ${newName}`);
+        })
+        .catch((error) => {
+          console.error("Error adding person:", error);
+        });
+    }
+  };
+
+  const handleDelete = (id) => {
+    const personToDelete = persons.find((person) => person.id === id);
+
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id !== id));
+          setDeletedPersons([
+            ...deletedPersons,
+            { id, name: personToDelete.name },
+          ]);
+          showNotification(
+            "success",
+            `${personToDelete.name} was successfully deleted.`
+          );
+        })
+        .catch((error) => {
+          console.error("Error deleting person:", error);
+        });
     }
   };
 
@@ -87,9 +187,30 @@ const App = () => {
     person.name.toLowerCase().includes(searchName.toLowerCase())
   );
 
+  const checkIfPersonDeleted = (name) => {
+    return deletedPersons.some((person) => person.name === name);
+  };
+
+  const showAlertIfPersonDeleted = (name) => {
+    if (checkIfPersonDeleted(name)) {
+      alert(
+        `Error: Information of ${name} has already been removed from the server.`
+      );
+    }
+  };
+
+  const showNotification = (type, text) => {
+    setNotificationMessage({ type, text });
+    setTimeout(() => {
+      setNotificationMessage(null);
+    }, 5000);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={notificationMessage} />
       <Filter
         searchTerm={searchName}
         handleSearchNameChange={handleSearchNameChange}
@@ -103,7 +224,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons filteredPersons={filteredPersons} handleDelete={handleDelete} />
     </div>
   );
 };

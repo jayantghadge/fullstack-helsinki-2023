493cd03fea47f755f4c39a456d101a9ffcfe454a
Jayant Ghadge <jayantghadge6@gmail.com>
2024-01-07 11:41:51 +0530

    Added 4.1 - 4.2 exercises of Part 4

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,69 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+// fetching all blog records
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+// fetching specific blog using id
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+// adding a new blog
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  });
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// updating a blog
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const updatedBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
